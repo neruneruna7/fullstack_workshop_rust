121bd5710c24f561c7eb9cda118cc2f914422622
neruneruna7 <113534341+neruneruna7@users.noreply.github.com>
2023-11-30 16:07:08 +0900

    add: database connection

--- api/shuttle/src/main.rs
+++ api/shuttle/src/main.rs
@@ -1,5 +1,9 @@
-use actix_web::{get, web::ServiceConfig};
+use actix_web::{get, post, web::{self, ServiceConfig}, HttpResponse, Responder, Result};
 use shuttle_actix_web::ShuttleActixWeb;
+use shuttle_runtime::CustomError;
+
+use sqlx::PgPool;
+
 
 #[get("/")]
 async fn hello_world() -> &'static str {
@@ -7,7 +11,9 @@ async fn hello_world() -> &'static str {
 }
 
 #[shuttle_runtime::main]
-async fn main() -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clone + 'static> {
+async fn actix_web(
+    #[shuttle_shared_db::Postgres()] pool: PgPool,
+) -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clone + 'static> {
     let config = move |cfg: &mut ServiceConfig| {
         cfg.service(hello_world);
     };
