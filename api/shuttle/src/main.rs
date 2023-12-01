71701e5de5106c8ac76f6e0e6c1ece1b48336b2f
neruneruna7 <113534341+neruneruna7@users.noreply.github.com>
2023-12-01 10:04:21 +0900

    add: health checl endpoints

--- api/shuttle/src/main.rs
+++ api/shuttle/src/main.rs
@@ -1,13 +1,14 @@
-use actix_web::{get, post, web::{self, ServiceConfig}, HttpResponse, Responder, Result};
+use actix_web::{
+    web::{self, ServiceConfig},
+};
 use shuttle_actix_web::ShuttleActixWeb;
 use shuttle_runtime::CustomError;
+
 use sqlx::Executor;
-use shuttle_secrets::SecretStore;
 
 use sqlx::PgPool;
 
-
-use api_lib::health::{hello_world, version, ping};
+use api_lib::health::{health, hello_world};
 
 #[shuttle_runtime::main]
 async fn actix_web(
@@ -19,15 +20,15 @@ async fn actix_web(
 ) -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clone + 'static> {
     // let database_url = secret_store.get("DATABASE_URL").unwrap();
     // let pool = PgPool::connect(&database_url).await.unwrap();
-    let _ = pool.execute(include_str!("../../db/schema.sql")).await.map_err(CustomError::new).unwrap();
+    let _ = pool
+        .execute(include_str!("../../db/schema.sql"))
+        .await
+        .map_err(CustomError::new)
+        .unwrap();
     let pool = web::Data::new(pool);
 
     let config = move |cfg: &mut ServiceConfig| {
-        cfg
-        .app_data(pool)
-        .service(hello_world)
-        .service(version)
-        .service(ping);
+        cfg.app_data(pool).service(hello_world).service(health);
     };
 
     Ok(config.into())
